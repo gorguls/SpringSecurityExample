9d4bac3af0091a94573d154049274ffd6624c10c
Сергей Елисеев <yeliseyev.serg@gmail.com>
2025-05-23 20:53:26 +0300

    Геттеры сеттеры конструкторы

--- src/main/java/com/example/springsecurityaxample/DB/DAO/User.java
+++ src/main/java/com/example/springsecurityaxample/DB/DAO/User.java
@@ -19,5 +19,36 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Set<Roles>  roles;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Roles> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Roles> roles) {
+        this.roles = roles;
+    }
+
+
     public User() {}
+
+    public User(String username, String password, Set<Roles> roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
 }

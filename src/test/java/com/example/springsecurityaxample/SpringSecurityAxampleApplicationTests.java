c194c11f9fbd7a7dd34f5330ad341ed11dc35eea
Сергей Елисеев <yeliseyev.serg@gmail.com>
2025-05-23 19:23:52 +0300

    START

---
+++ src/test/java/com/example/springsecurityaxample/SpringSecurityAxampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityaxample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAxampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

fb10b6d97bf2b3fb88fda207ea19e9b6beb04203
Сергей Елисеев <yeliseyev.serg@gmail.com>
2025-05-23 20:48:52 +0300

    Связали пользователей и роли

--- src/main/java/com/example/springsecurityaxample/DB/DAO/User.java
+++ src/main/java/com/example/springsecurityaxample/DB/DAO/User.java
@@ -2,6 +2,8 @@ package com.example.springsecurityaxample.DB.DAO;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "usrs")
 public class User {
@@ -11,4 +13,11 @@ public class User {
     private Long id;
     private String username;
     private String password;
+
+    @ElementCollection(targetClass = Roles.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Roles>  roles;
+
+    public User() {}
 }

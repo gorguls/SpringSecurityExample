1b0620b2a572ff9c97e92f27684eb2c144975053
Сергей Елисеев <yeliseyev.serg@gmail.com>
2025-05-24 13:00:44 +0300

    Много чего: секурити конфиг, страницы логин регистрация

---
+++ src/main/java/com/example/springsecurityaxample/Configurations/WebSeurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.springsecurityaxample.Configurations;
+
+import com.example.springsecurityaxample.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSeurityConfig{
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/registration").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/example/springsecurityaxample/Controllers/registrationController.java
@@ -0,0 +1,41 @@
+package com.example.springsecurityaxample.Controllers;
+
+import com.example.springsecurityaxample.DB.DAO.Repositories.UserRepository;
+import com.example.springsecurityaxample.DB.DAO.Roles;
+import com.example.springsecurityaxample.DB.DAO.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Controller
+public class registrationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/registration")
+    public String registrationPage() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Model model) {
+        User userGromDB = userRepository.findByUsername(user.getUsername());
+        if (userGromDB != null) {
+            model.addAttribute("errorMessage", "This username is already taken");
+            return "registration";
+        }
+
+        user.setActiv(true);
+        user.setPassword(user.getPassword());
+        user.setRoles(Collections.singleton(Roles.USER));
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
